0b50a20f15671bdb5dbc0be999e3293da7c9740e
LuisSid55 <luiscitty4@gmail.com>
2021-09-26 20:52:20 -0300

    Juros Simples

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, i, t, j, m;
+            Console.WriteLine("\n--Juros Simples--\n");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Para calcular o juros simples, Digite:\n");
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Capital (R$).....: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.ResetColor();
+            Console.Write("Taxa de juros (%): ");
+            i = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Tempo (Meses)....: ");
+            t = Convert.ToDouble(Console.ReadLine());
+
+            j = c * (i / 100) * t;
+
+            m = c + j;
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\nE o resultado é");
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"\nJuros...: {j:C}");
+            
+            Console.ResetColor();
+            Console.WriteLine($"Montante: {m:C}\n");
+        }
+    }
+}
